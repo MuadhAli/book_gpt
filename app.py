820895444594e5387b53098a05a13c46c6cfb5e7
MuadhAli <mkhanmanzil@gmail.com>
2023-04-30 12:34:03 +0530

    update last

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, session
+import os
+import tempfile
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain import OpenAI, VectorDBQA
+from langchain.document_loaders import DirectoryLoader
+from langchain.document_loaders import UnstructuredPDFLoader
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+app = Flask(__name__)
+app.secret_key = os.urandom(24)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    question = request.form.get('query') or request.args.get('query') or session.get('question', '')
+    session['question'] = question
+    result = None
+
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+
+        if uploaded_file.filename != '':
+            # Save the uploaded file to a temporary file on disk
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(uploaded_file.read())
+                temp_file.flush()
+
+                # Load the PDF from the temporary file
+                loader = UnstructuredPDFLoader(temp_file.name)
+                documents = loader.load()
+                text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+                texts = text_splitter.split_documents(documents)
+                embeddings = OpenAIEmbeddings(openai_api_key=os.environ['OPENAI_API_KEY'])
+                docsearch = Chroma.from_documents(texts, embeddings)
+                qa = VectorDBQA.from_chain_type(llm=OpenAI(), chain_type="stuff", vectorstore=docsearch)
+
+                query = question
+
+                result = qa({"query": query})
+
+    return render_template('index.html', question=question, result=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/script.js
@@ -0,0 +1,62 @@
+document.getElementById('mode-toggle-btn').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
+
+
+document.getElementById("mode-slider").addEventListener("change", function() {
+    document.body.classList.toggle("dark-mode");
+});
+
+document.getElementById("send-btn").addEventListener("click", function() {
+    sendMessage();
+});
+
+document.getElementById("user-input").addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+        sendMessage();
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+function sendMessage() {
+    var userInput = document.getElementById("user-input");
+    var userMessage = userInput.value.trim();
+    if (userMessage !== "") {
+        var chatBody = document.getElementById("chat-body");
+        var userChatMessage = document.createElement("div");
+        userChatMessage.className = "chat-message";
+        userChatMessage.innerHTML = '<div class="chat-message-inner user"><p>' + userMessage + '</p></div>';
+        chatBody.appendChild(userChatMessage);
+        userInput.value = "";
+        userInput.focus();
+        chatBody.scrollTop = chatBody.scrollHeight;
+        setTimeout(function() {
+            var assistantChatMessage = document.createElement("div");
+            assistantChatMessage.className = "chat-message";
+            assistantChatMessage.innerHTML = '<div class="chat-message-inner assistant"><p>Hi! How can I help you?</p></div>';
+            chatBody.appendChild(assistantChatMessage);
+            chatBody.scrollTop = chatBody.scrollHeight;
+        }, 1000);
+    }
+}
+
+        // Code to be executed when enter key is pressed
+        // e.g., form submission or event triggering
+    }
+});
+
+
+function populateQuestion() {
+    // Set the value of the user-input field
+    document.getElementById('user-input').value = 'Your question for Pitch Creator';
+    // Submit the form
+    document.getElementById('send-btn').click();
+  }
+
+  function populateQuestion1() {
+    // Set the value of the user-input field
+    document.getElementById('user-input').value = 'Your question for Blurb Creator';
+    // Submit the form
+    document.getElementById('send-btn').click();
+  }
